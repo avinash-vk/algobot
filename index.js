dd757af6bd6fab70afd893685c718ee6d93264ca
avinash-vk <avinash2000vk@gmail.com>
2021-01-06 20:07:10 +0530

    added basic bot code!

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// Configuring env
+require("dotenv").config();
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+client.login(process.env.DISCORD_TOKEN);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
