fd3bfe201241f255daac4626eb59f98a96a41555
avinash-vk <avinash2000vk@gmail.com>
2021-01-12 21:29:10 +0530

    basic bot commands

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const client = new Discord.Client();
 // Connecting it to the appropriate endpoint
 client.login(process.env.DISCORD_TOKEN);
 
+// Custom bot replies
+const REPLIES = require("./replies.js");
+
 /*
   *********** BOT COMMANDS ************
 */
@@ -17,7 +20,14 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg && msg.content[0] === ";"){
+    let content = msg.content.substring(1).trim().toLowerCase();
+    if (content === "intro") {
+      msg.reply(REPLIES.intro);
+    }
+    else if (content === "dm") {
+      msg.react("♋");
+      msg.author.send(REPLIES.dm);
+    }
   }
 });
---
+++ replies.js
@@ -0,0 +1,6 @@
+const REPLIES = {
+  intro: "Hello world, I'm algobot! I am gonna help you FAANG your interviews and become better at what you're supposed to be good at. Let me warn you, even though I'll be here trying to motivate you, in the end its upto you to have fun and learn at every step. Be patient, Rome wasn't build overnight and neither was facebook(hopefully not, eitherways you aren't Mark).You got this.",
+  dm:"Howdy, how's your day going?"
+}
+
+module.exports = REPLIES
