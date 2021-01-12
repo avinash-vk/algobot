e636e356451979a8aff228cd352a3bf42ffbae26
avinash-vk <avinash2000vk@gmail.com>
2021-01-12 18:46:49 +0530

    intermediate

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Configuring env
 require("dotenv").config();
 
+// Initialising the app
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Connecting it to the appropriate endpoint
 client.login(process.env.DISCORD_TOKEN);
 
+/*
+  *********** BOT COMMANDS ************
+*/
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`BOT READY!`);
 });
 
 client.on('message', msg => {
