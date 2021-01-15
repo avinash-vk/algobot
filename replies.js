e7622d79b1b8e3a147ee740193b68a175080a0b7
avinash-vk <avinash2000vk@gmail.com>
2021-01-15 22:33:27 +0530

    tmp changes :

--- replies.js
+++ replies.js
@@ -1,6 +1,6 @@
 const REPLIES = {
   intro: "Hello world, I'm algobot! I am gonna help you FAANG your interviews and become better at what you're supposed to be good at. Let me warn you, even though I'll be here trying to motivate you, in the end its upto you to have fun and learn at every step. Be patient, Rome wasn't build overnight and neither was facebook(hopefully not, eitherways you aren't Mark).You got this.",
-  dm:"Howdy, how's your day going?;)",
+  dm:"Howdy, how's your day going?",
   help:"Well I'm still under development. Help me help you more by helping yourself.",
   default:"I didn't quite get you there, master. Would you be down to break it into something I could better interpret?\nIf you're not sure what this is about, type `;help` and know more about me!",
   challenge_me:"I am proud that you've challenged yourself into solving that one more question. Remember every second you contribute to this feat matters, and I hope you gain nothing but motivation to do more,to do better.\n",
